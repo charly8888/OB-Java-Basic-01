a05395f781b2c176860729555bc32dd34ab74e8e
German Hornus <hornusgerman@gmail.com>
2023-02-15 18:00:22 -0600

    Finish task

---
+++ src/main/Main.java
@@ -0,0 +1,33 @@
+package main;
+
+public class Main {
+    public static void main(String[] args){
+
+        //letters and words
+        char letter = 'S';
+        String name = "Sofía";
+
+        //numbers
+        int ten = 10;
+        double twentyPointFiftyFour = 20.54d;
+        long manyNumbers = 12345678901234567L;
+        float thirteenPointFive = 31.5221f;
+        byte two = 127;
+
+        // booleans
+        boolean falsy = false;
+
+
+        System.out.println("Character " + letter);
+        System.out.println("String " + name);
+        System.out.println("Number integer " + ten);
+        System.out.println("Number double " + twentyPointFiftyFour);
+        System.out.println("Number long " + manyNumbers);
+        System.out.println("Number float " + thirteenPointFive);
+        System.out.println("Number byte " + two);
+        System.out.println("boolean " + falsy);
+
+
+
+    }
+}
